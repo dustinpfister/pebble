fa5f5f3b3a337fa478bad65ba506b07748542617
Dustin John Pfister <dustin.pfister@gmail.com>
2016-03-22 18:55:29 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,35 @@
+# Pebble
+
+Pebble is a social incremental game in which you gain pebbles from a main world pebble 'reserve' account, as well as other players 'wallet' accounts into your own personal wallet account. You also pay out pebbles to other players accounts, and the reserve in exchangedfor services. You also pay pebbles back to the reserve in the form of taxes.
+
+
+## Getting started
+
+To get started set up your pebble account by registering on the sign-up page. Once You create your account and log in you will have some starting pebbles. You can start manually gaining pebbles from the reserve by going to the reserve tab and clicking the 'take a pebble' button which allows you to directly take pebbles from the reserve account one at a time.
+
+## Pebble income :
+
+There are lots of ways to gain pebbles, some ways involve taking them dirrectly from the social reserve account, other ways involve reciving them from other players.
+
+    From the reserve:
+
+    * Press the 'take a pebble' button at the reserve tab.
+    * Apply for welfare ( no shame in it if you are just starting out, or went broke).
+    * Log in at least once a day to gain a daily bonus for playing often.
+    
+    
+    From other players:
+    
+    * gain pebbles from other players that visit your shops at the shops tab.
+    * A player just gives you pebbles for whatever the reason. ( maybe they are just nice? )
+    
+    
+## Spending Pebbles :
+
+Pebbles are ment to be spent not horded, if you hord to many pebbles your taxes will go up allot, so spend them on stuff!
+
+    * build and improve shops at the shop page.
+    
+## End of day
+
+At the end of a game day welfare is payed out, tax rates are computed, and then collected.
---
+++ server.js
@@ -0,0 +1,165 @@
+// #!/bin/env node
+
+/*    Pebble
+ *    Copyright 2016 by Dustin Pfister (GPL-3.0)
+ *    dustin.pfister@gamil.com
+ *    
+ *    https://github.com/dustinpfister/pebble
+ *
+ *    try to get them all!
+ */
+
+var express = require('express'),
+session = require('express-session'),
+MongoStore = require('connect-mongo/es5')(session)
+openShift = require('./lib/openshift.js').openShiftObj,
+
+// passport
+passport = require('passport'),
+Strategy = require('passport-local').Strategy,
+
+// express app
+app = express(),
+    
+    
+// users
+users = require('./lib/users.js'),
+    
+// pebble lib
+pebble = require('./lib/pebble.js'),
+
+// use passport local strategy
+// following example at : https://github.com/passport/express-4.x-local-example/blob/master/server.js
+passport.use(new Strategy(
+
+    function(username, password, cb) {
+
+        users.findByUsername(username, function(err, user) {
+
+            if (err) {
+                return cb(err);
+            }
+            if (!user) {
+                return cb(null, false);
+            }
+            if (user.password != password) {
+                return cb(null, false);
+            }
+            return cb(null, user);
+        });
+
+    }
+
+
+));
+
+passport.serializeUser(function(user, cb) {
+    cb(null, user.id);
+});
+
+passport.deserializeUser(function(id, cb) {
+
+    users.findById(id, function(err, user) {
+
+        if (err) {
+            return cb(err);
+        }
+
+        cb(null, user);
+    });
+
+});
+
+// Use application-level middleware for common functionality, including logging, parsing, and session handling.
+app.use(require('cookie-parser')());
+app.use(require('body-parser').json({limit: '5mb'}));
+app.use(require('body-parser').urlencoded({extended: true,limit: '5mb'}));
+app.use( session({
+    secret: 'keyboard cat', // ALERT! look into express-session and why the secret is important
+    resave: false,
+    store: new MongoStore({
+        url: openShift.mongo
+    }),
+    saveUninitialized: false,
+    limit: '5mb'
+}));
+app.use(passport.initialize());  // Initialize Passport and restore authentication state, if any, from the session
+app.use(passport.session());
+
+// use EJS for rendering
+app.set( 'view engine', 'ejs' );
+app.use( express.static('views') ); // must do this to get external files
+
+
+app.get('*', function(req, res, next){
+     
+    var visitPaths = ['/login', '/signup'], // paths that are okay to visit without being logged in
+    i =0, len = visitPaths.length, okay;
+    
+    // check if logged in
+    if(req.user){
+    
+        next();
+        
+    // redirrect to login page
+    }else{
+        
+        console.log(req.path);
+        
+        i=0;
+        okay = false;
+        while(i < len){
+            if(req.path === visitPaths[i]){
+                okay = true;
+                break;
+            }
+            i++;
+        }
+
+        // if not okay redirrect
+        if(!okay){
+            res.redirect('/login')
+        }else{
+            next();
+        }
+        
+    }
+    
+});
+
+app.get('/', function(req, res, next){
+
+    res.render('main', {});
+    
+});
+
+app.get('/login', function(req, res, next){
+
+    res.render('login', {});
+    
+});
+
+app.get('/signup', function(req, res, next){
+
+    res.render('signup', {});
+    
+});
+app.post('/signup', function(req, res, next){
+    
+    users.newUser(req,res);
+    
+});
+
+// start the server
+app.listen(openShift.port, openShift.ipaddress, function(){
+
+    console.log('pebble lives');
+
+    users.infoCheck();
+    pebble.reserveCheck();
+    
+    // for now create an account, and call pebble.takeRequest
+    //pebble.takeRequest('dustin', 1);
+    
+    
+});
