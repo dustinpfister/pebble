eef58338b06328d61404d42bf14352866a801ec9
Dustin John Pfister <dustin.pfister@gmail.com>
2016-06-15 16:17:57 -0400

    removed commented out code

--- server.js
+++ server.js
@@ -404,8 +404,6 @@ app.get('/signup', function (req, res, next) {
 
 app.post('/signup', function (req, res, next) {
 
-    // users.newUser(req, res);
-
     users.newUser(req, 
                   
         function(){
